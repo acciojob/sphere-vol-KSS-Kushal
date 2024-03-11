fd666708f6c74474d204fb04662fff35a99f543d
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-11 20:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+	const value = document.getElementById("radius").value;
+	const ans = document.getElementById("volume");
+	if (isNaN(value) || value<0) {
+		ans.value = NaN;
+	}
+	const v = (4/3)*3.014*value*value*value;
+	ans.value = v.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
